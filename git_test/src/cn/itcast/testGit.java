3b8e813fdcbd32f63624162c3982e7dfc1b74f24
lsl_glb <1796008778@qq.com>
2018-11-26 19:27:29 +0800

    first update class

--- git_test/src/cn/itcast/testGit.java
+++ git_test/src/cn/itcast/testGit.java
@@ -3,8 +3,10 @@ package cn.itcast;
 public class testGit {
     public static void main(String[] args) {
         System.out.println("Hello,Git!");
+        //git test
     }
     public void test(){
         int i= 1+10;
+        System.out.println(i);
     }
 }
